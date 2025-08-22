0e2d9191366894c9b9028cfe5afcfa2dbacbcee2
Fiboliriodem <101954899+Fiboliriodem@users.noreply.github.com>
2025-08-22 16:04:01 -0300

    paginas de login e profile

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,16 @@
+import styles from './style.module.css';
+
+export function Login() {
+  return (
+    <>
+      <div className={styles.header}>
+        <h1>TESTE</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, quis
+          sed ab dolorem pariatur provident rem ut et vitae, repellendus minima
+          fuga alias nam autem ea consequatur adipisci earum ad ipsum nihil.
+        </p>
+      </div>
+    </>
+  );
+}
--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { Home } from '../../pages/Home';
+import { Login } from '../../pages/Login';
+import { Profile } from '../../pages/Profile';
 
 export function MainRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/home/' element={<Home />} />
+        <Route path='/profile/' element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
