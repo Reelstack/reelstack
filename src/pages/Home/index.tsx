bd59da057e6c8987dec1742c7b975c1f9f24f72f
Felipe Vasconcelos Cardoso <101954899+Fiboliriodem@users.noreply.github.com>
2025-09-03 20:01:16 -0300

    iteração inicial do poster

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,38 @@
+import { useState } from 'react';
 import { RouterLink } from '../../components/RouterLink';
 import styles from './style.module.css';
+import { AnimatePresence, motion } from 'motion/react';
 
 export function Home() {
+  const [isHover, setHover] = useState(false);
+
   return (
     <>
-      <div className={styles.header}>
-        <h1>BeelBack</h1>
-        <p>
-          Welcome to BeelBack
-        </p>
+      <div className={styles.wrapper}>
+        <div
+          className={styles.poster}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          <AnimatePresence mode='wait'>
+            {isHover && (
+              <motion.div
+                key='info'
+                initial={{ opacity: 0, y: +50 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: +50 }}
+                transition={{ duration: 0.4, ease: 'easeInOut' }}
+              >
+                <div className={styles.info}>
+                  <h1>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Fuga, suscipit.
+                  </h1>
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
         <RouterLink href='/'>LOGIN</RouterLink>
         <RouterLink href='/profile/'>PROFILE</RouterLink>
       </div>
