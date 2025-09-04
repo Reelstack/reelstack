bb901cc32f9c5b1102bec9880ac2fc77cfa5a5a7
Felipe Vasconcelos Cardoso <101954899+Fiboliriodem@users.noreply.github.com>
2025-09-04 19:37:54 -0300

    template temporário com link para home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { RouterLink } from '../../components/RouterLink';
 import styles from './style.module.css';
 import { AnimatePresence, motion } from 'motion/react';
+import cdown from '../../assets/chevron-down.svg';
+import { RouterLink } from '../../components/RouterLink';
 
 export function Home() {
   const [isHover, setHover] = useState(false);
@@ -11,6 +12,7 @@ export function Home() {
       <div className={styles.wrapper}>
         <div
           className={styles.poster}
+          // hover de informações
           onMouseEnter={() => setHover(true)}
           onMouseLeave={() => setHover(false)}
         >
@@ -18,23 +20,32 @@ export function Home() {
             {isHover && (
               <motion.div
                 key='info'
-                initial={{ opacity: 0, y: +50 }}
+                initial={{ opacity: 0, y: +200 }}
                 animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: +50 }}
-                transition={{ duration: 0.4, ease: 'easeInOut' }}
+                exit={{ opacity: 0, y: +200 }}
+                transition={{ duration: 0.5, ease: 'easeInOut' }}
               >
                 <div className={styles.info}>
-                  <h1>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Fuga, suscipit.
-                  </h1>
+                  <div style={{ textAlign: 'center' }}>
+                    <h1>Goncharov</h1>
+                    <h2>Action, Drama, Suspense</h2>
+                  </div>
+
+                  <h2>Director: Martin Scorcese</h2>
+                  <h3>
+                    Main Cast: Robert De Niro, Al Pacino, Gene Hackman, Harvey
+                    keitel, John Cazale, Cybill Sheperd
+                  </h3>
+                  <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <RouterLink href='/'>
+                      <img className={styles.cdown} src={cdown} />
+                    </RouterLink>
+                  </div>
                 </div>
               </motion.div>
             )}
           </AnimatePresence>
         </div>
-        <RouterLink href='/'>LOGIN</RouterLink>
-        <RouterLink href='/profile/'>PROFILE</RouterLink>
       </div>
     </>
   );
