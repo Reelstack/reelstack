538a88344c566e9f37dcb354728b3ec706a36295
Felipe Vasconcelos Cardoso <101954899+Fiboliriodem@users.noreply.github.com>
2025-11-03 20:32:28 -0300

    atualizaçao do restricted para não rodar duas vezes o algoritmo

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import { MainRouter } from './routers/MainRouter';
 
 import { Toaster } from 'react-hot-toast';
 import TestAuth from './tests/TestAuth';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { testUserRecommendations } from './tests/TestAlgoritm';
 
 export default function App() {
+  const ranOnce = useRef(false);
+
   useEffect(() => {
+    if (ranOnce.current) return;
+    ranOnce.current = true;
+
+    // your code that should run only once
     testUserRecommendations();
   }, []);
   return (
